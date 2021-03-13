e74e59d7096f78b0f3b39a3ba322c1df67bd5c3b
yousef-ghanim <yousefghanim85@gmail.com>
2021-03-13 19:37:28 +0000

    adding files

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        animi quo nostrum quaerat minima voluptatum quis atque, similique itaque
+        ex provident eius officiis. Aut perspiciatis natus totam, vitae
+        voluptatum dicta ratione saepe, nihil ut odio adipisci provident
+        deserunt nulla in.
+      </p>
+    </section>
+  );
+};
+
+export default About;
