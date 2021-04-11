09a5d075dcb0440e6c8bf29a9f8b581db0a684bb
yousef-ghanim <yousefghanim85@gmail.com>
2021-04-11 12:30:39 +0000

    Updates

---
+++ precache-manifest.7a50b30fd128c1d6cef040ee372b3d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4c9bc9a3d6207dd522b0f8d3ca6728",
+    "url": "/cocktails-react/index.html"
+  },
+  {
+    "revision": "89070d212ebdb4bee954",
+    "url": "/cocktails-react/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "68e9d93a1dfba5322ade",
+    "url": "/cocktails-react/static/js/2.e42185e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktails-react/static/js/2.e42185e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89070d212ebdb4bee954",
+    "url": "/cocktails-react/static/js/main.c7f1e467.chunk.js"
+  },
+  {
+    "revision": "84fe1d3337b96e3cb640",
+    "url": "/cocktails-react/static/js/runtime-main.db503d2c.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktails-react/static/media/logo.9a3d2645.svg"
+  }
+]);
